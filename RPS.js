3a42dddd73de6c166d5e7aade0520d60b7610faf
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2023-11-19 10:45:20 -0800

    Created 2 variables for random number and random string. Random func generates a number but neeeded a string output.

--- RPS.js
+++ RPS.js
@@ -7,5 +7,8 @@ const rpsList = [
 
 
 function computerChoice () {
-   
+   let randomNumber = Math.floor(Math.random()*rpsList.length);
+   let randomString = rpsList[randomNumber];
+   return randomString;
 }
+alert(computerChoice());
