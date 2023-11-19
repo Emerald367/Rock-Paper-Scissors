8c01f221aa4a4e9a43edc3fb807e0b6b5e55fc11
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2023-11-19 14:01:34 -0800

    Completion of 2nd Function with testing.

--- RPS.js
+++ RPS.js
@@ -6,9 +6,42 @@ const rpsList = [
       
 
 
-function computerChoice () {
+function computerChoice() {
    let randomNumber = Math.floor(Math.random()*rpsList.length);
    let randomString = rpsList[randomNumber];
    return randomString;
 }
-alert(computerChoice());
+
+
+//CALL 1 Round of Rock, Paper, Scissors\\
+//PARAMETERS for Player and Computer\\
+//IF (certain outcome) THEN declare player winner or loser\\
+
+
+function playRound(playerSelection, computerSelection) {
+    const LOSING_STATEMENT = "You Lose!"
+    const WINNING_STATEMENT = "You Win!"
+    const DRAW = "Tie!"
+    
+
+    if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return LOSING_STATEMENT;
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return LOSING_STATEMENT;
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return LOSING_STATEMENT;
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return WINNING_STATEMENT;
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return WINNING_STATEMENT;
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return WINNING_STATEMENT;
+    } else {
+        return DRAW;
+    }
+          
+}
+
+ const playerSelection = "Rock";
+let computerSelection = computerChoice();
+console.log(playRound(playerSelection, computerSelection));
